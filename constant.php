aca58ecf9d9076b1147abc178a15afb5cc17b969
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2022-09-12 23:39:18 +0530

    update Index page

---
+++ constant.php
@@ -0,0 +1,5 @@
+<?php
+//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
+define('SITE_KEY',"6LfbKqkhAAAAAEFLP2deGzGLNLleTashf9EckTFY"); 
+define('SECRET_KEY',"6LfbKqkhAAAAAM4CTdJ0rgYMbPC7nRme-HpQ_OlV");
+?>
